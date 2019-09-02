f843a8965a98cfe7e48c485a2e1caaa412ce602e
newelco <54794309+newelco@users.noreply.github.com>
2019-09-02 16:35:30 +0200

    Create settings.h

---
+++ settings.h
@@ -0,0 +1,75 @@
+/*----------------------------------------------------------------------------------------------------
+  Project Name : Solar Powered WiFi Weather Station V2.31
+  Features: temperature, dewpoint, dewpoint spread, heat index, humidity, absolute pressure, relative pressure, battery status and
+  the famous Zambretti Forecaster (multi lingual)
+  Authors: Keith Hungerford, Debasish Dutta and Marc Stähli
+  Website : www.opengreenenergy.com
+
+******* configuration control constant for use of Blynk and/or Thingspeak ***/
+
+const String App1 = "BLYNK";         // empty string if not applicable -> "" else "BLYNK" 
+const String App2 = "THINGSPEAK";    // empty string if not applicable -> "" else "THINGSPEAK"
+
+
+/****** Blink or ThingSpeak Settings ****************************************/
+
+char auth[] = "XXX"; // Blynk Auth Token
+
+char ssid[] = "XXX";                           // WiFi Router ssid
+char pass[] = "XXX";             // WiFi Router password
+
+const char* server = "api.thingspeak.com";        // Thingspeak Write API
+const char* api_key = "XXX";         // API write key 
+
+/****** MQTT Settings ********************************************************/
+
+//const char* mqtt_server = "192.xxx.xxx.xxx";      // MQTT Server (broker) address
+
+
+/****** Additional Settings **************************************************/
+
+#define LANGUAGE 'PL'               //either 'DE' for German or 'EN' for English
+
+#define TEMP_CORR (0)              //Manual correction of temp sensor (mine reads 1 degree too high)
+#define ELEVATION (90)             //Enter your elevation in m ASL to calculate rel pressure (ASL/QNH) at your place
+
+#define sleepTimeMin (3)           //setting of deepsleep time in minutes (default: 10)
+
+// NTP   --> Just a remark - the program needs the time only for the timestamp, so for the Zambretti forecast
+//           the timezone and the DST (Daylight Saving Time) is irrelevant. This is why I did not take care of DST 
+//           in the code. I saw a fork on Github (truckershitch) which I believe has covered this.
+
+#define NTP_SERVER      "ch.pool.ntp.org"
+#define TZ              1           // (utc+) TZ in hours
+#define DST_MN          60          // use 60mn for summer time in some countries
+
+#define TZ_SEC          ((TZ)*3600)  // don't change this
+#define DST_SEC         ((DST_MN)*60)// don't change this
+
+/**********Blynk & ThingSpeak assginments ---------------------------------
+
+Blynk:
+
+virtual pin 0 Temperature (Celcius)
+virtual pin 1 Humidity (%)
+virtual pin 2 Absolute Pressure (hPa)
+virtual pin 3 Relative Pressure (hPa)
+virtual pin 4 Battery Volts (V)
+virtual pin 5 Dewpoint (Celcius)
+virtual pin 6 HeatIndex (Celcius)
+virtual pin 7 Zambrettis Words
+virtual pin 8 Accuracy in percent (%)
+virtual pin 9 Tend in Words
+virtual pin 10 Dewpoint Spread
+
+ThingSpeak:
+
+Field 1: Relative Pressure (hPa)
+Field 2: Temperature (Celcius)
+Field 3: Humidity (%)
+Field 4: Battery (V)
+Field 5: Absolute Pressure (hPa)
+Field 6: Dewpoint (Celcius)
+Field 7: HeatIndex (Celcius) 
+
+***************************************************************************/
